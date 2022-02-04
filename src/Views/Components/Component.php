165cd4bea0c09d3b00bcd5c3bc44308f4ba5611c
Amro Khaled <wshcylinder@gmail.com>
2022-02-04 17:35:08 +0200

    flush

--- src/Views/Components/Component.php
+++ src/Views/Components/Component.php
@@ -6,5 +6,12 @@ use Illuminate\View\Component as BaseComponent;
 
 abstract class Component extends BaseComponent
 {
+    public $defaultAttributes;
+
     abstract public function render();
+
+    public function __construct()
+    {
+        $this->defaultAttributes = $this->defaultAttributes ?: $this->newAttributeBag();
+    }
 }
