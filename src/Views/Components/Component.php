ea9049f127c0c45d16f0cb57d26a79378484a63a
Amro Khaled <wshcylinder@gmail.com>
2022-02-04 18:19:12 +0200

    :art:

--- src/Views/Components/Component.php
+++ src/Views/Components/Component.php
@@ -2,7 +2,9 @@
 
 namespace LDK\DashboardAbstract\Views\Components;
 
+use ReflectionClass;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\View;
 use Illuminate\View\Component as BaseComponent;
 
 abstract class Component extends BaseComponent
@@ -15,8 +17,6 @@ abstract class Component extends BaseComponent
         //
     }
 
-    abstract public function render();
-
     public function class(array $classes = [])
     {
         $this->class = Arr::toCssClasses($classes);
@@ -24,5 +24,16 @@ abstract class Component extends BaseComponent
         $this->defaultAttributes['class'] = $this->class;
 
         return $this;
+    }
+
+    public function render()
+    {
+        $componentName = strtolower((new ReflectionClass(get_called_class()))->getShortName());
+
+        if (View::exists("dashboard-ui::component.${componentName}")) {
+            return view("dashboard-ui::components.${componentName}");
+        }
+
+        return view("dashboard-abstract::components.${componentName}");
     }
 }
