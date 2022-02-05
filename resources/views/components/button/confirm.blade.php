95c5a230c72d9724d10fb1522717d36080b0fac7
Amro Khaled <wshcylinder@gmail.com>
2022-02-05 16:30:30 +0200

    :art: Confirm components are ready

--- resources/views/components/button/confirm.blade.php
+++ resources/views/components/button/confirm.blade.php
@@ -1,3 +1,6 @@
-<x-dashboard-btn wire:click="$emit('showConfirmAlert{{ $id }}')">
-    {!! $slot !!}
-</x-dashboard-btn>
+<x-dashboard::alert.confirm :id="$id"
+    :options="$options">
+    <x-dashboard-button :id="$id">
+        {!! $slot !!}
+    </x-dashboard-button>
+</x-dashboard::alert.confirm>
